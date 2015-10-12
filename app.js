d1ebd804d15c95c74a8e96a0abe1b3663981e676
behzadlashkari <bezlash@gmail.com>
2015-10-12 22:26:10 +0100

    added first round of files

---
+++ app.js
@@ -0,0 +1,28 @@
+
+(function (){ //JS closure
+  var app = angular.module('store', [ ]);
+
+  app.controller('StoreController', function(){ //sets name of controller as well as a function that will
+                                                // fire once the controller is called
+    this.products = gems;
+  });
+
+    var gems = [
+      { // This is a JS object stating the properties of "gem"
+        name: 'Diamond',
+        price: 2000,
+        description: 'Clear diamond, good quality colour',
+        canPurchase: true,  // Boolean used to show set the canPurchase value to true or false.  Used to show/hide add to cart button-->
+        soldOut: false,
+      },
+      { // This is a JS object stating the properties of "gem"
+        name: 'Ruby',
+        price: 1000,
+        description: 'Red ruby, good quality colour',
+        canPurchase: true,  // Boolean used to show set the canPurchase value to true or false.  Used to show/hide add to cart button-->
+        soldOut: false,
+      },
+
+    ];
+
+})();
