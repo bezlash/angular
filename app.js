e3ed257fc6bc1e7152babe175156649a68c2b473
Behzad Lashkari <bez@newdigitalpartnership.com>
2015-10-13 17:49:38 +0100

    added images

--- app.js
+++ app.js
@@ -10,17 +10,27 @@
     var gems = [
       { // This is a JS object stating the properties of "gem"
         name: 'Diamond',
-        price: 2000,
+        price: 2,
         description: 'Clear diamond, good quality colour',
         canPurchase: true,  // Boolean used to show set the canPurchase value to true or false.  Used to show/hide add to cart button-->
         soldOut: false,
+        images: [
+            "images/diamond-icon.jpg",
+            "images/diamond-icon2.jpeg",
+            "images/diamond-icon3.jpeg",
+        ],
       },
       { // This is a JS object stating the properties of "gem"
         name: 'Ruby',
-        price: 1000,
+        price: 1.95,
         description: 'Red ruby, good quality colour',
         canPurchase: false,  // Boolean used to show set the canPurchase value to true or false.  Used to show/hide add to cart button-->
         soldOut: false,
+        images: [
+            "images/ruby-icon.jpeg",
+            "images/ruby-icon1.jpeg",
+            "images/ruby-icon2.jpeg",
+        ],
       },
 
     ];
